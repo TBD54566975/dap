bb0f1c7419532c41185e84ecd2c080fa448efee2
Moe Jangda <moe@tbd.email>
2024-05-05 02:11:30 -0500

    add pseudo code for `RegisterDAP` handler

--- registry/handlers/register_dap.go
+++ registry/handlers/register_dap.go
@@ -14,5 +14,16 @@ type RegisterDAP struct {
 }
 
 func (h *RegisterDAP) Handle(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	/* TODO:
+	 * unmarshal request body into RegistrationRequest
+	 * check to ensure provided did's method is supported
+	 * verify signature
+	 * write to db
+	 * return 409 conflict if handle is already taken
+	 * sign registration request digest
+	 * construct RegistrationResponse
+	 * return 202 accepted
+	**/
+
 	w.WriteHeader(http.StatusNotImplemented)
 }
