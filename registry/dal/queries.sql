ff2ecb3c156489a85921eeef2d28d7e968876b85
Moe Jangda <moe@tbd.email>
2024-06-21 00:09:12 -0400

    remove code and add `CODEOWNERS`

--- registry/dal/queries.sql
+++
@@ -1,8 +0,0 @@
--- name: CreateDAP :exec
-INSERT INTO daps (id, did, handle, proof, date_created)
-  VALUES (?, ?, ?, ?, ?);
-
--- name: GetHandleDID :one
-SELECT * FROM daps
-  WHERE handle = ?
-  LIMIT 1;
